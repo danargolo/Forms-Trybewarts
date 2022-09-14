88a161cfec0a5c84abc84ff49d7a9983d3dce017
Seu nome <marcusaviotti@gmail.com>
2022-09-14 15:56:22 -0300

    Requisitos 1, 2 e 3 feitos

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.getElementById('btnForm');
+const formEmail = document.getElementById('email');
+const formSenha = document.getElementById('senha');
+
+function validaForm () {
+    if (formEmail.value === 'tryber@teste.com' && formSenha.value == '123456')
+        return alert('Olá, Tryber!');
+        else {
+            return alert('Email ou senha inválidos.');
+        }
+}
+
+button.addEventListener('click', validaForm);
+
